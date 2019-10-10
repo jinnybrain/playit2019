eb76e040092f248b289534e1d79d2fbc372d0ef0
seonguk <sukang@ubcare.co.kr>
2019-10-10 16:48:39 +0900

    Google Maps iframe link is changed in Location Component

--- playit2019/src/components/Location.js
+++ playit2019/src/components/Location.js
@@ -14,8 +14,7 @@ class Location extends Component {
                             <h5>마이크임팩트 12층 (1호선 종각역 4번 출구)</h5>
                         </div>
                         <div style={{width:'100%'}}>
-                        <iframe src={'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.7819848724616!2d127.05642241348355!3d37.513059834950056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca46bcb9e129f%3A0xd6bf8dde518b69a4!2z7L2U7JeR7Iqk!5e0!3m2!1sko!2skr!4v1569459592022!5m2!1sko!2skr'} style={{ width:'100%', height:'450px', frameborder:'0', border:'0'}}></iframe>
-
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12649.536528536628!2d126.98152400346366!3d37.569571367781286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca2e8dca48fdb%3A0xe6eeee574f549b43!2z66eI7J207YGs7J6E7Yyp7Yq4!5e0!3m2!1sko!2skr!4v1570693343478!5m2!1sko!2skr" style={{width:'100%', height:'450px', frameborder:'0', border:'0'}}></iframe>
                         </div>
                     </div>
                 </div>
