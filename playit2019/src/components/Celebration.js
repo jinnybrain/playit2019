31bf7a00b7c539d187e4c9c9dd770f7df96c8799
seonguk <sukang@ubcare.co.kr>
2019-10-17 13:12:51 +0900

    modify celebration component width for arrange layout

--- playit2019/src/components/Celebration.js
+++ playit2019/src/components/Celebration.js
@@ -9,15 +9,15 @@ class Celebration extends Component {
             <div className={'container'} style={{marginTop:'150px', marginBottom:'50px'}}>
                 <div className={'col-lg-12 text-left'}>
                     <h2 className={'section-heading text-uppercase'} style={{color:'#ff5d00'}}>축하 메시지 🎉</h2>
-                    <div className={'row'} style={{color:'#312d3e', marginTop:'50px', marginBottom:'70px', float:'left'}} >
+                    <div className={'row'} style={{color:'#312d3e', marginTop:'50px', marginBottom:'70px', float:'left', width:'100%'}} >
                         <img src={message0} style={{display:'block', width:'60%'}}  />
                     </div>
                  
-                    <div className={'row'} style={{color:'#312d3e', marginBottom:'70px', float:'left'}} >
+                    <div className={'row'} style={{color:'#312d3e', marginBottom:'70px', float:'left', width:'100%'}} >
                         <img src={message2} style={{display:'block', width:'60%'}}  />
                     </div>
 
-                    <div className={'row'} style={{color:'#312d3e', marginBottom:'70px', float:'left'}} >
+                    <div className={'row'} style={{color:'#312d3e', marginBottom:'70px', float:'left', width:'100%'}} >
                         <img src={message1} style={{display:'block',  width:'60%'}}  />
                     </div>
                 </div>
