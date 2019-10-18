dc81df1ed6be704cb555324a6f9961f333fda46a
seonguk <sukang@ubcare.co.kr>
2019-10-18 14:08:51 +0900

    mobx downgrade 5.x -> 4.14.0 for mobx proxy polyfill issue

--- playit2019/src/index.js
+++ playit2019/src/index.js
@@ -1,8 +1,9 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/stable';
 import MarketStore from './stores/market';
 import {Provider} from 'mobx-react';
 
