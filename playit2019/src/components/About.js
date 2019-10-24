7447479eb138f583b60eaa2539ed6fde5cf46e23
jinnybrain <codecaffein@ubcare.co.kr>
2019-10-24 21:04:01 +0900

    remove location and place.

--- playit2019/src/components/About.js
+++ playit2019/src/components/About.js
@@ -5,16 +5,16 @@ class About extends Component {
         return (
             <div className={'container'}>
                 <div className={'col-lg-12 text-left'}>
-                    <h2 className={'section-heading text-uppercase'} style={{color:'#F05125'}}>일시</h2>
+                    <h2 className={'section-heading text-uppercase'} style={{color:'#F05125'}}>일시 및 장소</h2>
                     <div className={'row'} style={{color:'#312d3e', marginTop:'20px', fontWeight:'50'}} >
                         <div className={'col-sm-12 text-left'} >
                             <h5 style={{fontWeight:100}}>2019년 11월 5일 금요일</h5>
                         </div>
                         <div className={'col-sm-12 text-left'} >
-                            <h5 style={{fontWeight:100}}>서울특별시 종로구 관철동 45-1 마이크임팩트 12층</h5>
+                            <h5 style={{fontWeight:100}}>  09:00 - 18:00 </h5>
                         </div>
                         <div className={'col-sm-12 text-left'} >
-                            <h5 style={{fontWeight:100}}>  09:00 - 18:00 </h5>
+                            <h5 style={{fontWeight:100}}>장소는 추후에 공지하겠습니다.</h5>
                         </div>
                     </div>
                 </div>
--- playit2019/src/components/Location.js
+++ playit2019/src/components/Location.js
@@ -8,10 +8,7 @@ class Location extends Component {
                     <h2 className={'section-heading text-uppercase'} style={{color:'#F05125'}}>장소</h2>
                     <div className={'row'} style={{color:'#312d3e', marginTop:'20px'}} >
                         <div className={'col-sm-12 text-left'} >
-                            <h5>서울특별시 종로구 관철동 45-1</h5>
-                        </div>
-                        <div className={'col-sm-12 text-left'} >
-                            <h5>마이크임팩트 12층 (1호선 종각역 4번 출구)</h5>
+                            <h5>장소 협의중</h5>
                         </div>
                         <div style={{width:'100%'}}>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12649.536528536628!2d126.98152400346366!3d37.569571367781286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca2e8dca48fdb%3A0xe6eeee574f549b43!2z66eI7J207YGs7J6E7Yyp7Yq4!5e0!3m2!1sko!2skr!4v1570693343478!5m2!1sko!2skr" style={{width:'100%', height:'450px', frameborder:'0', border:'0'}}></iframe>
