e0303b99d15cc4f95079b20244dd42b0c7f27d64
jinnybrain <codecaffein@ubcare.co.kr>
2019-10-25 12:55:46 +0900

    change location.

--- playit2019/src/components/About.js
+++ playit2019/src/components/About.js
@@ -8,13 +8,10 @@ class About extends Component {
                     <h2 className={'section-heading text-uppercase'} style={{color:'#F05125'}}>일시 및 장소</h2>
                     <div className={'row'} style={{color:'#312d3e', marginTop:'20px', fontWeight:'50'}} >
                         <div className={'col-sm-12 text-left'} >
-                            <h5 style={{fontWeight:100}}>2019년 11월 5일 금요일</h5>
+                            <h5 style={{fontWeight:100}}>2019년 11월 5일 금요일, 09:00~18:00</h5>
                         </div>
                         <div className={'col-sm-12 text-left'} >
-                            <h5 style={{fontWeight:100}}>  09:00 - 18:00 </h5>
-                        </div>
-                        <div className={'col-sm-12 text-left'} >
-                            <h5 style={{fontWeight:100}}>장소는 추후에 공지하겠습니다.</h5>
+                            <h5 style={{fontWeight:100}}>태평양물산1층 "레디:스페이스"에서</h5>
                         </div>
                     </div>
                 </div>
--- playit2019/src/components/Location.js
+++ playit2019/src/components/Location.js
@@ -5,13 +5,18 @@ class Location extends Component {
         return (
             <div className={'container'} style={{marginTop:'150px', marginBottom:'50px'}}>
                 <div className={'col-lg-12 text-left'}>
-                    <h2 className={'section-heading text-uppercase'} style={{color:'#F05125'}}>장소</h2>
+                    <h2 className={'section-heading text-uppercase'} style={{color:'#F05125'}}>오시는 길</h2>
                     <div className={'row'} style={{color:'#312d3e', marginTop:'20px'}} >
                         <div className={'col-sm-12 text-left'} >
-                            <h5>장소 협의중</h5>
+                            <h5>레디:스페이스 (LEDy space)</h5>
                         </div>
+                        <div className={'col-sm-12 text-left'} >
+                            <h5>서울시 구로구 구로동 197-21 번지 태평양물산 1층</h5>
+                        </div>
+                        
                         <div style={{width:'100%'}}>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12649.536528536628!2d126.98152400346366!3d37.569571367781286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca2e8dca48fdb%3A0xe6eeee574f549b43!2z66eI7J207YGs7J6E7Yyp7Yq4!5e0!3m2!1sko!2skr!4v1570693343478!5m2!1sko!2skr" style={{width:'100%', height:'450px', frameborder:'0', border:'0'}}></iframe>
+                        
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1916.7290753906573!2d126.8942082005725!3d37.4852480729009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c9f3e6de27889%3A0x434fd6cc09e464e2!2z66CI65SUOuyKpO2OmOydtOyKpA!5e0!3m2!1sko!2skr!4v1571975589568!5m2!1sko!2skr" style={{width:'100%', height:'450px', frameborder:'0', border:'0'}}></iframe>
                         </div>
                     </div>
                 </div>
